171aa0cd3f46e75128223f5b1a70a62b40002794
jo87jeff <jo87jimmy@gmail.com>
2025-09-21 21:01:58 +0800

    test4

--- main.py
+++ main.py
@@ -265,10 +265,17 @@ def main():
             # axes[1, 0].imshow(original_img, cmap='gray')  
             # axes[1, 0].imshow(high_confidence_mask, cmap='Reds', alpha=0.8, vmin=0, vmax=1)
 
-            # 使用更強烈的色彩映射  
+            # 二值化遮罩並找出輪廓  
+            binary_mask = (mask_overlay > 0.5).astype(np.uint8)  
+            contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  
             axes[1, 0].imshow(original_img, cmap='gray')  
-            im = axes[1, 0].imshow(mask_overlay, cmap='jet', alpha=0.7, vmin=0, vmax=1)  
-            plt.colorbar(im, ax=axes[1, 0], fraction=0.046, pad=0.04)
+            for contour in contours:  
+                axes[1, 0].plot(contour[:, 0, 0], contour[:, 0, 1], 'r-', linewidth=2)
+
+            # 使用更強烈的色彩映射  
+            # axes[1, 0].imshow(original_img, cmap='gray')  
+            # im = axes[1, 0].imshow(mask_overlay, cmap='jet', alpha=0.7, vmin=0, vmax=1)  
+            # plt.colorbar(im, ax=axes[1, 0], fraction=0.046, pad=0.04)
 
             axes[1, 0].set_title('Predicted Anomaly Overlay')
             axes[1, 0].axis('off')
